fad249a4368799089963abf4064fcb0f034c7e28
master7887 <urazaev.alexander@gmail.com>
2017-06-22 18:22:34 +0500

    fixed media queries bugs after mqpacker, compleate gulp.js

--- gulpfile.js
+++ gulpfile.js
@@ -32,7 +32,6 @@ gulp.task("style", function() {
     .pipe(minify())
     .pipe(rename("style.min.css"))
     .pipe(gulp.dest("build/css"))
-	
     .pipe(server.stream());
 });
 
@@ -42,7 +41,7 @@ gulp.task("images", function() {
     .pipe(imagemin([
       imagemin.optipng({optimizationLevel: 3}),
       imagemin.jpegtran({progressive: true})
-]))
+		]))
     .pipe(gulp.dest("build/img"));
 });
 
@@ -61,6 +60,7 @@ gulp.task("html:copy", function() {
   return gulp.src("*.html")
     .pipe(gulp.dest("build"));
 });
+
 gulp.task("html:update", ["html:copy"], function(done) {
   server.reload();
   done();
@@ -79,9 +79,15 @@ gulp.task("serve", function() {
 	gulp.watch("*.html", ["html:update"]);
 });
 
-gulp.task("build", function(fn){
-	run("style", "images", "symbols", fn);
-});
+//
+//gulp.task("serve", function() {
+//  server.init({
+//    server: "build/"
+//  });
+//  gulp.watch("less/**/*.less", ["style"]);
+//gulp.watch("*.html", ["html:update"]); });
+//
+
 
 gulp.task("copy", function() {
   return gulp.src([
