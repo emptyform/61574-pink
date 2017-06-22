830ef5710a05729acd99c3c3eaaa361e54c8c87e
Keks <keks@htmlacademy.ru>
2017-06-22 16:26:02 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/app.js
@@ -0,0 +1,27 @@
+var navMain = document.querySelector('.mainnav');
+var navToggle = document.querySelector('.mainnav__togler');
+var pageHeader = document.querySelector('.page-header');
+
+navMain.classList.remove('mainnav--nojs');
+pageHeader.classList.remove('page-header--nojs');
+
+navToggle.addEventListener('click', function () {
+	if (navMain.classList.contains('mainnav--closed')) {
+		navMain.classList.remove('mainnav--closed');
+		navMain.classList.add('mainnav--opened');
+		navToggle.classList.remove('mainnav__togler--closed');
+		navToggle.classList.add('mainnav__togler--open');
+		pageHeader.classList.remove('page-header--closed');
+		pageHeader.classList.add('page-header--open');
+	} 
+	else {
+		navMain.classList.add('mainnav--closed');
+		navMain.classList.remove('mainnav--opened');
+		navToggle.classList.remove('mainnav__togler--open');
+		navToggle.classList.add('mainnav__togler--closed');
+		pageHeader.classList.remove('page-header--open');
+		pageHeader.classList.add('page-header--closed');
+
+	}
+});
+
